3bc40a193cd4f1ebfd16521532b4cf9d4f31dc2a
JordanClemons <36278129+JordanClemons@users.noreply.github.com>
2021-01-21 17:29:30 -0500

    Setting up react side

--- index.js
+++ index.js
@@ -27,3 +27,11 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Mixing it up on port ${PORT}`)
 })
+
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
