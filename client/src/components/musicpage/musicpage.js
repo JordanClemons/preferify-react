7a1e5a9181202843678c5c17716c455ccf30ed71
JordanClemons <36278129+JordanClemons@users.noreply.github.com>
2021-01-23 01:23:06 -0500

    Added partial song list front end

--- client/src/components/musicpage/musicpage.js
+++ client/src/components/musicpage/musicpage.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import queryString from 'query-string';
+import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
+import Song from './song.js';
 import './musicpage.css';
 
+
+
 function MusicPage() {
+
+  var token = queryString.parse(window.location.search).access_token;
+  const [songs, setSongs] = useState([]);
+
+  const getSongs = () =>{
+    fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10',{
+    headers: {'Authorization': 'Bearer ' + token}
+    }).then(response => response.json())
+    .then(data => setSongs(data.items)) 
+}
+
+  useEffect(() =>{
+    getSongs();
+  }, []);
+
+  if(token == null){
+    return(
+      <Redirect to="/"></Redirect>  //If not logged in go back to login
+    )
+  }
+
   return (
-    <div className="container">Music</div>
+    <div>
+      <div className="music-container">
+        <div className="music-header-container">
+          <h1 className='music-header'>Preferify</h1>
+        </div>
+        <div className ="music-input-container">
+          <h1 className="music-top-header">Your top songs</h1>
+          <select className="dropdown">
+            <option value="50">10 songs</option>
+            <option value="100">25 songs</option>
+            <option value="500">50 songs</option>
+          </select>
+          <select className="dropdown">
+            <option value="1 month">1 month</option>
+            <option value="6 months">6 months</option>
+            <option value="all time">all time</option>
+          </select>
+        </div>
+        <div className="songs-container">
+          {songs.map((song) =>
+            <Song song={song}></Song>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ client/src/components/musicpage/song.js
@@ -0,0 +1,22 @@
+import React, {useEffect} from 'react';
+import './song.css';
+import Aos from 'aos';
+import "aos/dist/aos.css";
+
+
+function Song(song) {
+
+    useEffect(() => {
+        Aos.init({duration: 1000, offset:20});
+    }, []);
+
+  console.log(song.song);
+  return (
+    <div className="song-bubble" data-aos="fade-up">
+      <img className="album-art" src={song.song.album.images[0].url}></img>
+      <p1 className="song-title">{song.song.name}</p1>
+    </div>
+  );
+}
+
+export default Song;
